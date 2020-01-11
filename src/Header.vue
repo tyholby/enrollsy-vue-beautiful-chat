da42936a9aef11a740edcb5e60b7aa289029f8d0
Ty Holbrook <tytyholbrook@gmail.com>
2020-01-11 16:42:57 -0700

    Put (s) on Subscribed to # location(s)

--- src/Header.vue
+++ src/Header.vue
@@ -3,7 +3,7 @@
     <!--<img class="sc-header&#45;&#45;img" :src="imageUrl" alt="" v-if="imageUrl" />-->
     <div class="sc-header-users-text-container">
       <div class="sc-header--title" @click="toggleUserList"> {{title}} </div>
-      <div class="sc-open-drawer-text" @click="openHelpDrawer">Subscribed to {{ numberOfLocations }} locations</div>
+      <div class="sc-open-drawer-text" @click="openHelpDrawer">Subscribed to {{ numberOfLocations }} location(s)</div>
     </div>
     <div class="sc-header--close-button" @click="onClose">
       <img src="./assets/close-icon.png" alt="" />
